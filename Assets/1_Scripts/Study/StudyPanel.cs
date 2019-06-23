280003b03bd33b7610268acf1b3a5729d014464a
Taehyun Hwang <taehyun95@gmail.com>
2019-06-23 22:29:06 +0900

    Implement StudyPanel

--- Assets/1_Scripts/Study/StudyPanel.cs
+++ Assets/1_Scripts/Study/StudyPanel.cs
@@ -1,9 +1,48 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StudyPanel : MonoBehaviour
 {
+    [SerializeField] private Text _word;
+    [SerializeField] private Text[] _means;
+
+    private int _wordDataIndex;
+
     public void Show()
     {
+        _wordDataIndex = 0;
 
+        SetNextWord();
+    }
+
+    private void SetNextWord()
+    {
+        SetWordData(SceneManager.Instance.GetWordData(_wordDataIndex));
+
+        _wordDataIndex = (_wordDataIndex + 1) % SceneManager.Instance.GetWordDataCount();
+    }
+
+    private void SetWordData(WordData wordData)
+    {
+        _word.text = wordData.Word;
+
+        var i = 0;
+        var length = Math.Min(wordData.Means.Length, _means.Length);
+        for (i = 0; i < length; i++)
+        {
+            _means[i].gameObject.SetActive(true);
+            _means[i].text = wordData.Means[i];
+        }
+
+        for (; i < _means.Length; i++)
+        {
+            _means[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void OnBackgroundButtonClick()
+    {
+        SetNextWord();
     }
 }
