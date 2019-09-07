2d3dfa7361cc753b717e2fd8212ec34238e74fb7
Taehyun Hwang <taehyun95@gmail.com>
2019-09-07 17:12:19 +0900

    Implement StaticData class

---
+++ Assets/1_Scripts/StaticData/StaticData.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData : Singleton<StaticData>
+{
+    public static readonly int[] StudyPeriodDate = {2, 3, 5};
+    public static readonly int[] TestPeriodDate = {5, 10, 20};
+
+    public void Load()
+    {
+        LoadWordData();
+    }
+
+    public bool ShouldStudyNewWord(int date)
+    {
+        return date % 3 == 1 || date % 3 == 2;
+    }
+
+    #region WordData
+
+    private const string WordDataJsonPath = "WordData";
+
+    private readonly Dictionary<string, WordData> _wordData = new Dictionary<string, WordData>();
+
+    private void LoadWordData()
+    {
+        var text = Resources.Load<TextAsset>(WordDataJsonPath).text;
+        var wordData = JsonUtil.DeserializeObject<List<WordData>>(text);
+        foreach (var word in wordData)
+        {
+            _wordData[word.Spelling] = word;
+        }
+    }
+
+    #endregion
+}
