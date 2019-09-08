5de6fb253191bf3329ea9888126845a525eaa25a
Taehyun Hwang <taehyun95@gmail.com>
2019-09-08 18:53:27 +0900

    Fix SignIn and StudyWord requst so that there aren't duplicated study count

--- Assets/1_Scripts/Network/Request/SignInRequest.cs
+++ Assets/1_Scripts/Network/Request/SignInRequest.cs
@@ -21,14 +21,22 @@ public class SignInRequest : Request
         var isFirstLoginToday = now.Year != protocolUser.LastSignInTime.Year || now.DayOfYear != protocolUser.LastSignInTime.DayOfYear;
         protocolUser.LastSignInTime = now;
 
-        if (!isFirstLoginToday) return;
+        if (isFirstLoginToday)
+        {
+            UpdateTodayWords(protocolUser);
+        }
+    }
 
+    private void UpdateTodayWords(ProtocolUser protocolUser)
+    {
         protocolUser.TodayStudyDate++;
         protocolUser.TodayStudyWordsIndex = 0;
         if (StaticData.Instance.ShouldStudyNewWord(protocolUser.TodayStudyDate))
         {
             AddNewStudyWords(protocolUser);
         }
+
+        UpdateTodayWordStudyCount(protocolUser);
     }
 
     private void AddNewStudyWords(ProtocolUser protocolUser)
@@ -46,6 +54,26 @@ public class SignInRequest : Request
                 StudyCount = 0,
             };
             protocolUser.AddStudyWord(protocolUser.TodayStudyDate, id);
+        }
+    }
+
+    private void UpdateTodayWordStudyCount(ProtocolUser protocolUser)
+    {
+        var todayStudyWords = protocolUser.TodayStudyWords[protocolUser.TodayStudyDate];
+        foreach (var id in todayStudyWords)
+        {
+            var data = protocolUser.WordStudyData[id];
+            if (data.StudyCount >= StaticData.StudyPeriodDate.Length)
+            {
+                // TODO : Add word to test data
+            }
+            else
+            {
+                data.StudyCount++;
+
+                var nextStudyDate = protocolUser.TodayStudyDate + StaticData.StudyPeriodDate[data.StudyCount - 1];
+                protocolUser.AddStudyWord(nextStudyDate, data.Id);
+            }
         }
     }
 }
--- Assets/1_Scripts/Network/Request/StudyWordRequest.cs
+++ Assets/1_Scripts/Network/Request/StudyWordRequest.cs
@@ -3,7 +3,7 @@ public class StudyWorkRequest : Request
     protected override TResponse GetResponse<TResponse>()
     {
         var protocolUser = Server.Instance.GetProtocolUser();
-        UpdateWordStudyCount(protocolUser);
+        UpdateTodayStudyWordsIndex(protocolUser);
         Server.Instance.SaveProtocolUser();
 
         var response = new StudyWorkResponse()
@@ -13,23 +13,9 @@ public class StudyWorkRequest : Request
         return response as TResponse;
     }
 
-    private void UpdateWordStudyCount(ProtocolUser protocolUser)
+    private void UpdateTodayStudyWordsIndex(ProtocolUser protocolUser)
     {
         var todayStudyWords = protocolUser.TodayStudyWords[protocolUser.TodayStudyDate];
-        var id = todayStudyWords[protocolUser.TodayStudyWordsIndex];
         protocolUser.TodayStudyWordsIndex = (protocolUser.TodayStudyWordsIndex + 1) % todayStudyWords.Count;
-
-        var data = protocolUser.WordStudyData[id];
-        if (data.StudyCount >= StaticData.StudyPeriodDate.Length)
-        {
-            // TODO : Add word to test data
-        }
-        else
-        {
-            data.StudyCount++;
-
-            var nextStudyDate = protocolUser.TodayStudyDate + StaticData.StudyPeriodDate[data.StudyCount - 1];
-            protocolUser.AddStudyWord(nextStudyDate, data.Id);
-        }
     }
 }
