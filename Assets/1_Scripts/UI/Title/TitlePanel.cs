126b7e12b6c05c0f9fd1fd813be2eb64ff1b16b0
Taehyun Hwang <taehyun95@gmail.com>
2019-09-08 16:19:48 +0900

    Add day text ui to TitlePanel

--- Assets/1_Scripts/UI/Title/TitlePanel.cs
+++ Assets/1_Scripts/UI/Title/TitlePanel.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TitlePanel : MonoBehaviour
 {
+    [SerializeField] private Text _day;
+
     public void Show()
     {
+        _day.text = $"Day {User.Instance.TodayStudyDate}";
     }
 
     public void OnStudyButtonClick()
