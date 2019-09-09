bf4d14343d26c1a2aff962aa863e481794a0bca8
Taehyun Hwang <taehyun95@gmail.com>
2019-09-09 15:20:05 +0900

    Implement dictionary extension method

--- Assets/1_Scripts/Common/Extensions.cs
+++ Assets/1_Scripts/Common/Extensions.cs
@@ -1,10 +1,18 @@
 using System.Collections.Generic;
 
-public static class Extensions
+public static class KeyValuePairExtensions
 {
     public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
     {
         key = tuple.Key;
         value = tuple.Value;
+    }
+}
+
+public static class DictionaryExtensions
+{
+    public static TValue TryGetValueOrDefaultValue<TKey, TValue>(this Dictionary<TKey, TValue> thiz, TKey key)
+    {
+        return thiz.TryGetValue(key, out var value) ? value : default(TValue);
     }
 }
