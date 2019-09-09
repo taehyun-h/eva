1ae10e152df121b4db0d3170514052f8b46861a1
Taehyun Hwang <taehyun95@gmail.com>
2019-09-09 15:37:14 +0900

    Change button colors according as word exist in TitlePanel

--- Assets/1_Scripts/UI/Title/TitlePanel.cs
+++ Assets/1_Scripts/UI/Title/TitlePanel.cs
@@ -4,10 +4,27 @@ using UnityEngine.UI;
 public class TitlePanel : MonoBehaviour
 {
     [SerializeField] private Text _day;
+    [SerializeField] private Text _study;
+    [SerializeField] private Text _test;
 
     public void Show()
     {
         _day.text = $"Day {User.Instance.TodayStudyDate}";
+
+        UpdateStudyButton();
+        UpdateTestButton();
+    }
+
+    private void UpdateStudyButton()
+    {
+        var todayStudyWords = User.Instance.TodayStudyWords.TryGetValueOrDefaultValue(User.Instance.TodayStudyDate);
+        _study.color = todayStudyWords == null || todayStudyWords.Count == 0 ? Color.white : Color.yellow;
+    }
+
+    private void UpdateTestButton()
+    {
+        var todayTestWords = User.Instance.TodayTestWords.TryGetValueOrDefaultValue(User.Instance.TodayStudyDate);
+        _test.color = todayTestWords == null || todayTestWords.Count == 0 ? Color.white : Color.yellow;
     }
 
     public void OnStudyButtonClick()
