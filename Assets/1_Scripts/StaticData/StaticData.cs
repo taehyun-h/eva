99612655946123ed183fc10b5f3a974c1d67ad21
Taehyun Hwang <taehyun95@gmail.com>
2019-11-12 11:06:40 +0900

    Implement loading word proto file in StaticData

--- Assets/1_Scripts/StaticData/StaticData.cs
+++ Assets/1_Scripts/StaticData/StaticData.cs
@@ -1,12 +1,49 @@
 using System.Collections.Generic;
 using System.Linq;
+using Data.Word;
+using UnityEngine;
 
 public class StaticData : Singleton<StaticData>
 {
+    public StaticData()
+    {
+        LoadWord();
+    }
+
     public void Load(string wordDataText)
     {
         LoadWordData(wordDataText);
     }
+
+    #region Word
+
+    private const string WordDataPath = "Data/WordData";
+    private readonly Word _defaultWord = new Word
+    {
+        Id = -1,
+        Spelling = "Not Found"
+    };
+
+    private Words _words;
+
+    public Word GetWord(int id)
+    {
+        return _words.Words_.ContainsKey(id) ? _words.Words_[id] : _defaultWord;
+    }
+
+    private void LoadWord()
+    {
+        var wordData = Resources.Load<TextAsset>(WordDataPath);
+        if (wordData == null)
+        {
+            _words = new Words();
+            return;
+        }
+
+        _words = Words.Parser.ParseFrom(wordData.bytes);
+    }
+
+    #endregion
 
     #region WordData
 
